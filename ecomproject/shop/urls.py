4ceae4e469aabff405e31c123dc1f09feee0d2cf
Shilu <shiluufrank@gmail.com>
2024-04-28 10:58:24 +0530

    changed

---
+++ ecomproject/shop/urls.py
@@ -0,0 +1,11 @@
+from . import views
+from django.urls import path, include
+
+urlpatterns = [
+    path("", views.home, name='home'),
+    path('<slug:slug_c>/', views.home, name='product_by_category'),
+    path('<slug:slug_c>/<slug_p>/', views.prod_det, name="product_det"),
+
+
+]
+
